7f486d76ae62c654d91f4c954c00645bc91733eb
jllloydd <wayfivet@gmail.com>
2024-04-13 20:42:31 +0800

    Add: Foreign key 'user_id' for appointments so that which users made which appointments are known.'

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,22 +5,35 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Appointment;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
     public function store(Request $request)
     {
 
-        $validatedData = $request->validate([
+        $userid = Auth::user()->id;
+
+        $input = $request->all();
+
+        $request->validate([
             'name' => 'required|string|max:255',
             'course' => 'required|string|max:255',
             'mode' => 'required|string|max:255',
             'gender' => 'required|string|max:255',
             'date' => 'required|date',
-            'time' => 'required|string|max:255'
+            'time' => 'required|string|max:255',
         ]);
 
-        Appointment::create($validatedData);
+        Appointment::create([
+            'name' => $input['name'],
+            'course' => $input['course'],
+            'mode' => $input['mode'],
+            'gender' => $input['gender'],
+            'date' => $input['date'],
+            'time' => $input['time'],
+            "user_id" => $userid
+        ]);
 
         return redirect('dashboard')->with('status', 'Appointment created successfully! You may now check the status of your appointment.');
     }
